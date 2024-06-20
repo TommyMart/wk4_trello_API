93b0c09deab89ccfc364035d90b76196f18886a1
TommyMart <15423@coderacademy.edu.au>
2024-06-20 19:52:54 +0930

    added cards

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -33,6 +33,7 @@ def register_user():
 
         # respond back
         return user_schema.dump(user), 201
+    
     except IntegrityError as err:
         if err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
             # not ful violation
